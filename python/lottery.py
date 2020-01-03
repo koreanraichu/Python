ab8fd7090b0ecf1e266cab01e81f9a1df7745cac
koreanraichu <blackholekun@gmail.com>
2020-01-04 01:00:29 +0900

    중복 어떻게 처리하죠

---
+++ python/lottery.py
@@ -0,0 +1,10 @@
+import random
+lotto=["0","1","2","3","4","5"]
+lotto[0]=random.randrange(1,64)
+lotto[1]=random.randrange(1,64)
+lotto[2]=random.randrange(1,64)
+lotto[3]=random.randrange(1,64)
+lotto[4]=random.randrange(1,64)
+lotto[5]=random.randrange(1,64)
+print(lotto)
+#중복 어떻게 처리하지...

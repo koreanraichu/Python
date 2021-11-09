ef4abb560e9ca20bd93557422eb6330e11a7b751
koreanraichu <blackholekun@gmail.com>
2021-11-09 16:41:38 +0900

    아 풀었다!!!
    리스트 밖으로 빼니까 해결됨...

--- python/practice/codingtest_sol.py
+++ python/practice/codingtest_sol.py
@@ -22,34 +22,36 @@ convert_fasta=SeqIO.convert(handle, "fastq", "/home/koreanraichu/sra_data_mo.fas
 # A2. FASTA file에서 A, T, G, C 세기
 # FASTA파일을 다시 불러올 예정이다.
 handle2="/home/koreanraichu/sra_data_mo.fasta"
+A = [] #아니 이게 이렇게 해결된다고?
+T = [] # 리스트 밖으로 뺐더니 이게 돼?????
+G = []
+C = []
 for record2 in SeqIO.parse(handle2,"fasta"):
-    print(Seq(record2.seq).count("A"))
-    print(type(Seq(record2.seq).count("A")))
-    # Count로 세는 건 되는데 합계를 안 내준다. int형인데 더해주질 않음.
+    A.append(Seq(record2.seq).count("A"))
+    T.append(Seq(record2.seq).count("T"))
+    G.append(Seq(record2.seq).count("G"))
+    C.append(Seq(record2.seq).count("C"))
+print("A: ",sum(A),"T: ",sum(T),"G: ",sum(G),"C: ",sum(C))
 
 # A3. 파일에 수록된 전체 시퀀스 길이
 print(sum(len(r) for r in SeqIO.parse(handle2, "fasta")))
 
 # A4. Top 10 length
+record_id=[] # 야 이걸 이렇게 해결보네
+record_len=[]
 for record2 in SeqIO.parse(handle2,"fasta"):
-    record_id=np.array(record2.id)
-    record_len=np.array(len(record2.seq))
-    record_table=pd.DataFrame({"ID":record_id, "length":record_len},index=[0])
-    print(record_table)
-    # Dataframe이 이상하게 생성된다.
-    """
-                   ID  length
-    0  SRR000021.37.2     249
-    이런 식으로 항목 하나당 한 데이터프레임이 생성되는데 Array까지는 정상적으로 생성됨. 
-    """
+    record_id.append(record2.id)
+    record_len.append(len(record2.seq))
+record_id_array=np.array(record_id)
+record_len_array=np.array(record_len)
+# 배열화 성공(위에 빼 둔 시르트로 생성함)
+record_table=pd.DataFrame({"ID":record_id_array,"Length":record_len_array})
+record_table2=record_table.groupby('Length').count()
+record_table2=record_table2.sort_values('Length',ascending=0)
+print(record_table2.head(10))
+# 아 드디어 깔끔한 표가 나왔습니다!!!
+# 그리고 어쨌든 Top10 뽑았음
 
 # A5. 조건부 합계(여기서는 100bp 이상/미만으로 나눈다)
-for record2 in SeqIO.parse(handle2,"fasta"):
-    if len(record2.seq) > 100:
-        i=0
-        i=i+len(record2.seq)
-    else:
-        j=0
-        j=j+len(record2.seq)
-print(i,j)
-# 이것도 합계가 구해지는 게 아니라 맨 마지막 값이 더해진다.
+print("length >= 100bp:",record_table['Length'][record_table.Length >= 100].sum())
+print("length < 100bp:",record_table['Length'][record_table.Length < 100].sum())

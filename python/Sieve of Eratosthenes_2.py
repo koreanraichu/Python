720ebeef20b200e538836caa86195dcc353951bd
koreanraichu <blackholekun@gmail.com>
2022-01-19 15:34:28 +0900

    제곱근 버전 추가
    에라토스테네스의 체로 나눠보는 수의 기준은 루트(n)입니다. (이때 n은 소수를 찾고자 하는 수 범위의 최댓값)

--- python/Sieve of Eratosthenes_2.py
+++ python/Sieve of Eratosthenes_2.py
@@ -3,9 +3,10 @@ N = int(sys.stdin.readline())
 prime = []
 
 def isprime(a):
+    sqrt = int(a ** 0.5)
     if a < 2:
         return False
-    for i in range(2,a):
+    for i in range(2,sqrt+1):
         if a % i == 0:
             return False
     else: 

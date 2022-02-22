b7359e5dd10f7d5efd68b01fc4000ef0c488d9d7
koreanraichu <blackholekun@gmail.com>
2022-02-22 22:48:21 +0900

    아무튼 내가 생각한 로직대로 나오긴 함
    키보드...가 안될때 마우스가 ^[[C ^]]D를 왕창 띄우는데 대체 뭔 상황인지...

--- python/perfectnumber.py
+++ python/perfectnumber.py
@@ -3,8 +3,8 @@ a = int(sys.stdin.readline())
 yaksu = []
 for i in range(1,a+1):
     if a % i == 0:
-        yaksu.append(str(i))
-yaksu_text = ', '.join(yaksu)
+        yaksu.append(i)
+yaksu_text = ''.join(str(yaksu))
 print('{}의 약수는 {}입니다. '.format(a,yaksu_text))
 if sum(yaksu) == 2 * a:
     print('{}는 자기 자신을 제외한 약수들의 합이 {}와 동일하므로 완전수입니다. '.format(a,a))

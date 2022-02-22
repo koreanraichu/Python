6ecbf4d14a9873b5260576fdc636553980b6a4e2
koreanraichu <blackholekun@gmail.com>
2022-02-22 22:38:49 +0900

    일단  생각한 대로 짜긴 했습니다...
    근데 키보드 진짜 왜이러냐...

--- python/perfectnumber.py
+++ python/perfectnumber.py
@@ -4,5 +4,7 @@ yaksu = []
 for i in range(1,a+1):
     if a % i == 0:
         yaksu.append(str(i))
-yaksu = ', '.join(yaksu)
-print('{}의 약수는 {}입니다. '.format(a,yaksu))
+yaksu_text = ', '.join(yaksu)
+print('{}의 약수는 {}입니다. '.format(a,yaksu_text))
+if sum(yaksu) == 2 * a:
+    print('{}는 자기 자신을 제외한 약수들의 합이 {}와 동일하므로 완전수입니다. '.format(a,a))

9f9ff115c0bd06cb88aeb315672990aca242e28e
koreanraichu <blackholekun@gmail.com>
2022-02-23 22:38:11 +0900

    완전수 코드 형식을 좀 수정했습니다.
    솔직히 리스트 그대로 나오면 뵈기 싫잖음... 
    
    +미끄메라 코드에 복숭아맛 추가함

--- python/perfectnumber.py
+++ python/perfectnumber.py
@@ -1,10 +1,15 @@
 import sys
 a = int(sys.stdin.readline())
 yaksu = []
+yaksu_text = []
 for i in range(1,a+1):
     if a % i == 0:
         yaksu.append(i)
-yaksu_text = ''.join(str(yaksu))
+        yaksu_text.append(str(i))
+yaksu_text = ', '.join(yaksu_text)
 print('{}의 약수는 {}입니다. '.format(a,yaksu_text))
-if sum(yaksu) == 2 * a:
-    print('{}는 자기 자신을 제외한 약수들의 합이 {}와 동일하므로 완전수입니다. '.format(a,a))
+sum = sum(yaksu)
+if sum == 2 * a:
+    print('{}는 자기 자신을 포함한 약수들의 합이 {}이고, {}와 동일하므로 완전수입니다. '.format(a,sum-yaksu[-1],a))
+else: 
+    print('{}는 자기 자신을 포함한 약수들의 합이 {}이고, {}와 다르므로 완전수가 아닙니다. '.format(a,sum-yaksu[-1],a))

3b80c11a87868c365faa577d11404c081c0f609a
koreanraichu <blackholekun@gmail.com>
2022-03-30 22:58:42 +0900

    팩토리얼 코드에 감마함수 이식했습니다.
    아 근데 얘는 복소수 지원 안해줌...

--- python/factorial_while.py
+++ python/factorial_while.py
@@ -1,5 +1,16 @@
+from sympy import *
+from mpmath import mp
 import sys
-a = float(sys.stdin.readline())
+
+def gamma_function(a):
+    t = symbols("t")
+    expr = t ** (a - 1) * exp(-t)
+    if a.real > 0:
+        return integrate((expr),(t,0,oo))
+    else: 
+        return False
+
+a = float(sys.stdin.readline().strip())
 # 와 팩토리얼이 생각보다 빡센거였구나... 
 # Factorial(계승): 일반적으로 n! = 1*2*3*...*n-1*n이다. (5!=1*2*3*4*5)
 factorial = 1
@@ -10,7 +21,7 @@ elif a == 0:
     factorial = 1
     # 0! = 1
 elif a % 1 != 0:
-    print("정수가 아닌 유리수는 일반적인 방법으로 팩토리얼을 구할 수 없습니다. ")
+    print(round(gamma_function(a+1),3))
 else: 
     while a >= 1:
         factorial *= a

f8147b6a1bdc88d7ff965645b870590201937715
koreanraichu <blackholekun@gmail.com>
2022-04-14 21:48:03 +0900

    아 안되네 코드 수정해야지...
    Spyder에서는 sys.stdin.readline()이 안되고 파이참은 심파이를 못부르고... VScode는 또 에러터지고... 어쩌라는걸까요... 
    
    아무튼 수정했습니다. 재귀함수 설정해둔거랑 변수명이 같아서 에러뜬듯...

--- python/factorial_recursion.py
+++ python/factorial_recursion.py
@@ -2,18 +2,18 @@ from sympy import *
 from mpmath import mp
 import sys
 
-def gamma_function(a):
+def gamma_function(n):
     t = symbols("t")
-    expr = t ** (a - 1) * exp(-t)
-    if a.real > 0:
+    expr = t ** (n - 1) * exp(-t)
+    if n.real > 0:
         return integrate((expr),(t,0,oo))
     else: 
         return False
 
-def factorial(a):
-  if a == 1:
+def factorial_RF(n):
+  if n == 1:
     return 1
-  return n * factorial(n-1)
+  return n * factorial_RF(n-1)
   
 a = float(sys.stdin.readline().strip())
 # Factorial(계승): 일반적으로 n! = 1*2*3*...*n-1*n이다. (5!=1*2*3*4*5)
@@ -27,4 +27,4 @@ elif a == 0:
 elif a % 1 != 0:
     print(round(gamma_function(a+1),3))
 else:
-    factorial(a)
+    print(factorial_RF(a))

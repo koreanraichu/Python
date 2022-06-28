56bb24c09dc89b302f4eee9df7787c9d9f326606
koreanraichu <blackholekun@gmail.com>
2022-06-28 16:51:44 +0900

    아코디언 패널(가로)
    Reference: https://ungdoli0916.tistory.com/784 ((즐거웅코드) 자바스크립트 누르면 펼쳐지는 아코디언 메뉴 구현하기)

---
+++ Javascript/accordion panel/script.js
@@ -0,0 +1,15 @@
+var acc = document.getElementsByClassName('accordion');
+var i = 0;
+
+for (i = 0;i < acc.length;i++) {
+    acc[i].addEventListener('click',function(e){
+        this.classList.toggle('active');
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        }
+        else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}

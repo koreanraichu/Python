1c84dfa8698d6444d7218c0dc83ebc4727c8237b
Koreanraichu <blackholekun@gmail.com>
2026-02-05 16:45:45 +0900

    출력 양식 수정... 여러분 우리가 뭘 찾는지는 알아야죠.

--- dbSNP in Entrez.py
+++ dbSNP in Entrez.py
@@ -1,12 +1,17 @@
 from Bio import Entrez
 Entrez.email = "blackholekun@gmail.com"
-handle = Entrez.esearch(db="snp", term="rs1050171", retmax="40" )
+handle = Entrez.esearch(db="snp", term="rs121434568", retmax="40")
 record = Entrez.read(handle)
 IdList=list(record['IdList'])
+print('=' * 100)
+
 # dbSNP+esearch
 for i in range(len(record['IdList'])):
     handle = Entrez.esummary(db="snp", id=IdList[i], retmode="xml")
-    records = Entrez.read((handle))
-    print(records['DocumentSummarySet']['DocumentSummary'][0]['GLOBAL_MAFS'][0])
-# 일단 esummary 써서 다 가져왔다. 근데 딕셔너리인데 왜 픽이 안될까...
-# 그 전에 저렇게 꼭 4단픽까지 가야겠냐... 아니 진짜 너무한 거 아닙니까...
+    records = Entrez.read(handle)
+    Gene = records['DocumentSummarySet']['DocumentSummary'][0]['GENES'][0]['NAME'] # 유전자 이름
+    chromosome = records['DocumentSummarySet']['DocumentSummary'][0]['CHR'] # 염색체
+    chrpos = records['DocumentSummarySet']['DocumentSummary'][0]['CHRPOS'] # 염색체 위치
+    spdi = records['DocumentSummarySet']['DocumentSummary'][0]['SPDI'] # NC_000007.14(시퀀스 어디):55206390(몇번째 염기):T(였던것):A(바뀐것)
+    Fxn_class = records['DocumentSummarySet']['DocumentSummary'][0]['FXN_CLASS'] # 그래서 얘가 어디 변이임?
+    print(f'{Gene} | {chromosome} POS {chrpos} | {spdi}\n{Fxn_class}')

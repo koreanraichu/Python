1d0593ddba940d5b1caffccca2898e5aa2d8e5a2
Koreanraichu <blackholekun@gmail.com>
2026-02-05 16:14:45 +0900

    파이참이 줄 그어놔서 뭐 잘못된 줄 알았습니다.. 바뀐건 없어요.

--- 99bottlesofbeer.py
+++ 99bottlesofbeer.py
@@ -8,7 +8,7 @@ for bottle in range (99,0,-1):
         else:
             lyric = "1 bottle of beer on the wall"
     elif bottle == 1:
-         print("1 bottle of beer on the wall, 1 bottle of beer.")
-         lyric = "no more beer on the wall!"
+        print("1 bottle of beer on the wall, 1 bottle of beer.")
+        lyric = "no more beer on the wall!"
     print("Take one down, pass it around,", lyric)
 #실제로 맥주 이렇게 마시면 간이 욕합니다 여러분
